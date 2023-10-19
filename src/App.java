2a4bf9b98e8310ca661a92197c313b953a4403d9
prabhjot-arora31 <83449967+prabhjot-arora31@users.noreply.github.com>
2023-10-19 19:19:08 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,11 +3,6 @@ import javax.script.ScriptEngineManager;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
-        // ScriptEngineManager manager = new ScriptEngineManager();
-        // ScriptEngine engine = manager.getEngineByName("js");
-        // Object res = engine.eval("2*3");
-        // System.out.println(res.toString());
         new UI("Calculator");
     }
 }
