0f2970311a4c6a9ae7618a18cb44769f93a261b5
prabhjot-arora31 <83449967+prabhjot-arora31@users.noreply.github.com>
2023-10-19 19:19:43 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,3 @@
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-
 public class App {
     public static void main(String[] args) throws Exception {
         new UI("Calculator");
